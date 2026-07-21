1bda1f279c18741c9030802876f37b8415bd61bd
Akhileswar Kamale <akhilkamale@gmail.com>
2026-07-21 15:47:58 +0530

    Stage 1: root and health endpoints

--- week2/main.py
+++ week2/main.py
@@ -7,4 +7,20 @@ def home():
     return {"message": "Hello World"}
 
 
+@app.get("/")
+def read_root():
+    return {
+        "name": "Task API",
+        "version": "1.0",
+        "endpoints":[
+            "/tasks"
+        ]
+    }
+
+@app.get("/health")
+def read_health():
+    return {"status": "ok"}
+
+
+
 
